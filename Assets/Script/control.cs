e2c37d2c8fd4c938371b52cdfb7b54ba01c19d3f
andriyan33 <aceprazif33@gmail.com>
2021-01-12 11:50:32 +0700

    Uas Prak Sismul 80%

---
+++ Assets/Script/control.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+	public float berat, tinggiLoncat;
+	public GameObject plane, intruction, pipeCopy, skor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown(){
+    	if (Time.timeScale == 1)
+    	{
+        GameObject.Find ("suara wing").GetComponent<AudioSource> ().Play ();
+        pipeCopy.SetActive (true);
+        intruction.SetActive (false);
+        skor.SetActive (true);
+    	plane.GetComponent<Rigidbody2D> ().gravityScale = berat;
+    	plane.GetComponent<Rigidbody2D> ().velocity = new Vector2 (plane.GetComponent<Rigidbody2D> ().velocity.x, tinggiLoncat);
+ 		}
+		}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Escape))
+        {
+            Application.Quit ();
+        }
+    }
+}
